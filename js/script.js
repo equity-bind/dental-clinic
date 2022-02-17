3ca872f7db009165815e6f60cbbd828ad6a5e91d
Vortigern <>
2022-02-17 17:40:24 +0900

    トップ画面完成、ヘッダーロゴ・ハンバーガーメニュー完成(メニュー未完成)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-  var topBtn = $('.pagetop');
+  var topBtn = $(".pagetop");
   topBtn.hide();
 
   // ボタンの表示設定
@@ -17,34 +17,36 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   // ボタンをクリックしたらスクロールして上に戻る
   topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300,
+      "swing"
+    );
     return false;
   });
 
   //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
+  $(".js-hamburger").on("click", function () {
+    if ($(".js-hamburger").hasClass("open")) {
+      // $(".js-drawer-menu").removeClass("open");
+      $(this).removeClass("open");
+    } else {
+      // $(".js-drawer-menu").addClass("open");
+      $(this).addClass("open");
+    }
   });
-
-
 
   // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
 
-  $(document).on('click', 'a[href*="#"]', function () {
+  $(document).on("click", 'a[href*="#"]', function () {
     let time = 400;
-    let header = $('header').innerHeight();
+    let header = $("header").innerHeight();
     let target = $(this.hash);
     if (!target.length) return;
     let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    $("html,body").animate({ scrollTop: targetY }, time, "swing");
     return false;
   });
-
 });
